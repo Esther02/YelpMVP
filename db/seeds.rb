0d400c1d027292229581b2b7a20f5bce4275df35
Esther Carminatti <yunalesca002@gmail.com>
2021-11-10 14:03:09 +0000

    controller created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+initialize 'faker'
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Faker::Restaurant.type,
+  )
+  puts "Creating a new restaurant called #{restaurant.name}"
+end
