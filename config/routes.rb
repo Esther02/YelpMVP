85ea9ff6d3d290666d7d998300b331945305551f
Esther Carminatti <yunalesca002@gmail.com>
2021-11-10 15:53:01 +0000

    views done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # A visitor can see the list of all restaurants.
-  get '/restaurants', to: 'restaurant#index'
+  get '/restaurants', to: 'restaurants#index'
   # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
-  get '/restaurants/new', to: 'restaurant#new', as: :new_restaurant
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
   post '/restaurants', to: 'restaurants#create'
   # A visitor can read one restaurant.
-  get '/restaurants/:id', to: 'restaurant#show', as: :restaurant
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
   # A visitor can add a new review to a restaurant
   get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
   post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+
+  resources :reviews, only: [:new, :create]
 end
